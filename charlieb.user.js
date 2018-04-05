80e4ce13bc536306950d2c7c9aeaccaa97fceb68
CalvT <calvin@jstomkins.com>
2018-04-05 16:51:44 -0300

    Acknowledge commands

--- charlieb.user.js
+++ charlieb.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CharlieB
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       CalvT
 // @match        https://chat.stackexchange.com/rooms/11540/charcoal-hq
@@ -12,7 +12,11 @@
 (function() {
     'use strict';
 
-    var charlieRegex = /^@Cha(\b|r(\b|l(\b|i(\b|e(\b|B)))))\b/;
+    var charlieRegex = /^@cha(|r(|l(|i(|e(|b)))))\b/i;
+
+    var messageRegex = /[^a-z0-9\s._-]/gi;
+
+    var commandRegex = /^\s*[a-z]*\b/i;
 
     var shutupOn = 0;
 
@@ -29,8 +33,11 @@
             var charlieCheck = content.match(charlieRegex);
             // Check if message is to Charlie
             if (charlieCheck !== null) {
-
-                console.log(event_type + " $$$ " + user_id + " $$$ " + content);
+                // Prepare content for Charlie
+                var charlieMessage = content.replace(charlieRegex, "").replace(messageRegex, "");
+                var charlieCommand = charlieMessage.trim().match(commandRegex);
+                var charlieParameters = charlieMessage.trim().replace(commandRegex, "");
+                chatMessage("I recieved the command *" + charlieCommand + "* along with the parameters *" + charlieParameters + "* from user " + user_id);
 
             }
 
@@ -39,17 +46,17 @@
     }
 
     function chatMessage(message) {
-        if (shutupOn === 0) {
-            document.getElementById('input').value = "[ [CharlieB](https://github.com/CalvT/CharlieB) ] " + message;
-            $('#sayit-button').click();
-        } else if (shutupOn !== 0) {
-            console.log('CB: ' + message);
-        }
+    if (shutupOn === 0) {
+        document.getElementById('input').value = "[ [CharlieB](https://github.com/CalvT/CharlieB) ] " + message;
+        $('#sayit-button').click();
+    } else if (shutupOn !== 0) {
+        console.log('CB: ' + message);
     }
+}
 
     function startup(){
         $.get("https://api.github.com/repos/CalvT/CharlieB/git/refs/heads/master", function(ghjson){
-            chatMessage("CharlieB started at revision [" + ghjson.object.sha.substring(0, 6) + "](https://github.com/CalvT/CharlieB/commit/" + ghjson.object.sha +") on CalvT/SurfacePro");
+            chatMessage("CharlieB started at revision [" + ghjson.object.sha.substring(0, 6) + "](https://github.com/CalvT/CharlieB/commit/" + ghjson.object.sha +") on CalvT/SurfaceBook");
         });
 
     }
