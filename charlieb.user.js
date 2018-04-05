a9fd7243b3d60702c81c2dce19aca6054ac03f43
CalvT <calvin@jstomkins.com>
2018-04-05 06:58:39 -0300

    Add message function

--- charlieb.user.js
+++ charlieb.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CharlieB
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       CalvT
 // @match        https://chat.stackexchange.com/rooms/11540/charcoal-hq
@@ -13,6 +13,8 @@
     'use strict';
 
     var charlieRegex = /^@Cha(\b|r(\b|l(\b|i(\b|e(\b|B)))))\b/;
+
+    var shutupOn = 0;
 
     $(document).ready(function () {
         // Add function to the CHAT event handler:
@@ -36,13 +38,18 @@
 
     }
 
-    function charlieMessage(message) {
-        console.log("[CharlieB] " + message);
+    function chatMessage(message) {
+        if (shutupOn === 0) {
+            document.getElementById('input').value = "[ [CharlieB](https://github.com/CalvT/CharlieB) ] " + message;
+            $('#sayit-button').click();
+        } else if (shutupOn !== 0) {
+            console.log('CB: ' + message);
+        }
     }
 
     function startup(){
         $.get("https://api.github.com/repos/CalvT/CharlieB/git/refs/heads/master", function(ghjson){
-            charlieMessage("CharlieB started at revision [" + ghjson.object.sha.substring(0, 6) + "](https://github.com/CalvT/CharlieB/commit/" + ghjson.object.sha +") on CalvT/SurfacePro");
+            chatMessage("CharlieB started at revision [" + ghjson.object.sha.substring(0, 6) + "](https://github.com/CalvT/CharlieB/commit/" + ghjson.object.sha +") on CalvT/SurfacePro");
         });
 
     }
